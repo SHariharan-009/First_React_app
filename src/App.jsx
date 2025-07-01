64aec7d4aed1cd6169e2e10bb0e82ce44153dde0
Hariharan S <harisaravanan001@gmail.com>
2025-07-01 16:59:56 +0530

    share

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
+import { useState } from "react"
 
 
 
 function App() {
-  let x = 0
+
+
+  const [email, setEmail] = useState("")
+  const [password, SetPassword] = useState("")
+  console.log("email", email)
+  console.log("password", password)
+
+  const styles = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "12px"
+  }
+
+  const handleSubmit = () => {
+    const data = {
+      email: email,
+      password: password
+    }
+    console.log("data", data)
+  }
 
   return (
     <>
-     <h1>{x}</h1>
+      <div style={styles}  >
+        <input
+          placeholder="Enter Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder=" Enter Password"
+          value={password}
+          onChange={(e) => SetPassword(e.target.value)}
+        />
+        <button onClick={handleSubmit} className="bg-primary mt-3">Submit</button>
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,5 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    </StrictMode>
 )
